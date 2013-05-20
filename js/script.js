72a08aa8eb68ccbd54f5dd224ec702a30531093d
thebestsophist <bernardy@enguin.com>
2013-05-20 13:56:05 -0500

    Updating SASS and javascript, minor updates to conditional IE styles, changing jquery CDN, adding typography related scripts.

--- js/script.js
+++ js/script.js
@@ -4,23 +4,44 @@
 
 
 
+$(document).ready(function(){
+	$('body').removeClass('no-js');
+});
 
 
 
+/* Only < 48em */
+if (Modernizr.mq('only screen and (max-width: 767px)')) {
+
+}
+
+/* Anything > 48em */
+if (Modernizr.mq('only screen and (min-width: 768px)')) {
+
+}
+
+/* Between 48em and 60em */
+if (Modernizr.mq('only screen and (min-width: 768px) and (max-width: 960px)')) {
+
+}
+
+/* Anything > 60em */
+if (Modernizr.mq('only screen and min-width: 960px)')) {
+
+}
 
 
+/* High pixel density */
+if (window.devicePixelRatio >= 1.5) {
 
+}
 
+/* Portrait orientation */
+if (window.orientation == 0 || 180) {
 
+}
 
+/* Landscape orientation */
+if (window.orientation == 90 || 240) {
 
-
-
-
-
-
-
-
-
-
-
+}
